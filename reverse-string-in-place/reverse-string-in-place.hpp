8b42c2e899e478e3b9aceec982269289b45d6926
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-06 09:54:14 -0600

    Create reverse-string-in-place.hpp

---
+++ reverse-string-in-place/reverse-string-in-place.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+void reverse(string& str)
+{
+    // reverse the input string in place
+    
+
+}
