8e05a946dbebf1511c59b6e3659e16f63ae3a4dc
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-06 10:02:42 -0600

    Create merge-sorted-arrays.h

---
+++ merge-sorted-arrays/merge-sorted-arrays.h
@@ -0,0 +1,15 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+int * mergeArrays(const int *myArray, size_t myLength,
+                  const int *alicesArray, size_t alicesLength)
+{
+    // combine the sorted arrays into one large sorted array
+    
+
+    return NULL;
+}
