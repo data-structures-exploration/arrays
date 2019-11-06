a0c8ce66204525117955709804e37cf0e13f4337
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-06 10:05:39 -0600

    Create cafe-order-checker.h

---
+++ cafe-order-checker/cafe-order-checker.h
@@ -0,0 +1,15 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+int isFirstComeFirstServed(const int* takeOutOrders, size_t takeOutOrdersSize,
+                           const int* dineInOrders, size_t dineInOrdersSize,
+                           const int* servedOrders, size_t servedOrdersSize)
+{
+    // check if we're serving orders first-come, first-served
+    
+
+    return 0;
+}
