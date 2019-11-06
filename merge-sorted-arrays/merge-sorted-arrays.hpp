37c58a0ddd5215d048b8f2a35ae1c77e3192cfdc
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-06 10:01:32 -0600

    Create merge-sorted-arrays.hpp

---
+++ merge-sorted-arrays/merge-sorted-arrays.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+vector<int> mergeVectors(const vector<int>& myVector, const vector<int>& alicesVector)
+{
+    // combine the sorted vectors into one large sorted vector
+    
+
+    return vector<int>();
+}
