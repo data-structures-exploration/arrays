53a6a8c6f22749be63f8dd2a86dea1b4e61d2fff
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-06 10:04:46 -0600

    Create cafe-order-checker.hpp

---
+++ cafe-order-checker/cafe-order-checker.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+bool isFirstComeFirstServed(const vector<int>& takeOutOrders,
+                            const vector<int>& dineInOrders,
+                            const vector<int>& servedOrders)
+{
+    // check if we're serving orders first-come, first-served
+    
+
+    return false;
+}
